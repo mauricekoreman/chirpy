ac0fd38b91ff19fffd7e86385e42625d32c3a315
Maurice Koreman <mg.koreman99@gmail.com>
2025-04-07 23:25:26 +0200

    add some query params to getChirps handler

--- handlerGetChirps.go
+++ handlerGetChirps.go
@@ -1,17 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sort"
+
+	"github.com/google/uuid"
+)
 
 func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request) {
 	chirps, err := cfg.db.GetAllChirps(req.Context())
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "failed getting all chirps", err)
+		respondWithError(w, http.StatusInternalServerError, "failed retrieving chirps", err)
 		return
 	}
 
-	allChirps := []Chirp{}
+	authorID := uuid.Nil
+	authorIdString := req.URL.Query().Get("author_id")
+	if authorIdString != "" {
+		authorID, err = uuid.Parse(authorIdString)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "error parsing authorId", err)
+			return
+		}
+	}
 
+	allChirps := []Chirp{}
 	for _, chirp := range chirps {
+		if authorID != uuid.Nil && chirp.UserID != authorID {
+			continue
+		}
+
 		allChirps = append(allChirps, Chirp{
 			Body:      chirp.Body,
 			ID:        chirp.ID,
@@ -19,6 +37,11 @@ func (cfg *apiConfig) handlerGetChirps(w http.ResponseWriter, req *http.Request)
 			UpdatedAt: chirp.UpdatedAt,
 			UserId:    chirp.UserID,
 		})
+	}
+
+	sortString := req.URL.Query().Get("sort")
+	if sortString == "desc" {
+		sort.Slice(allChirps, func(i, j int) bool { return allChirps[i].CreatedAt.After(allChirps[j].CreatedAt) })
 	}
 
 	respondWithJSON(w, http.StatusOK, allChirps)
